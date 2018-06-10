64d8c37410db8a68dee67b59592414f84dbdaafc
Angeluz-07 <Alejandro_ramr777@hotmail.com>
2018-06-10 16:00:42 -0500

    Update decipher.py
    Added Caesar decipher of three letters back.

--- decipher.py
+++ decipher.py
@@ -1,3 +1,4 @@
+
 #decode caesar cipher of 3 letters forward
 def caesar_3forward(message):
 
@@ -34,6 +35,22 @@ def caesar_2forward(message):
                 decoded += chr(ord(character) + 26 - 2)
             else:     
                 decoded += chr(ord(character) - 2)
+        else:
+            decoded += character
+    return decoded
+
+#decode caesar cipher of three letters back
+def caesar_3back(message):
+    decoded = ""
+    for character in message:
+        #get ASCII decimal representation of only Uppercase Alphabet
+        if ord(character) in range(65, 91):
+            #check if is X,Y or Z
+            if ord(character)>=88 and ord(character)<=90:
+                #substract offset of 26 letters and add 3 positions
+                decoded += chr(ord(character) - 26 +3)
+            else:     
+                decoded += chr(ord(character) + 3)
         else:
             decoded += character
     return decoded
@@ -96,25 +113,29 @@ def combined(message):
     return caesar_3forward(atbash(a1z26(message)))
 
 
-
 mensaje = "PU. FDHVDULDQ ZLOO EH RXW QHAW ZHHN. PU. DWEDVK ZLOO VXEVWLWXWH. "
 print(caesar_3forward(mensaje))
 print()
 
+
 mensaje = "EWTUG AQW OCTKNAP"
 print(caesar_2forward(mensaje))
 print()
+
+
+mensaje = "TEV FP TBKAV PL MBOCBZQ"
+print(caesar_3back(mensaje))
+print()
+
 
 mensaje = "MLG S.T. DVOOH ZKKILEVW"
 print(atbash(mensaje))
 print()
 
 
-
 mensaje = "8-1-16-16-25 14-15-23, 1-18-9-5-12?"
 print(a1z26(mensaje))
 print()
-
 
 
 mensaje = "5-19-23-6-21-16 18-9-6 4-16-19 22-12-15-10-20-19-25-19"
@@ -124,7 +145,6 @@ print()
 
 mensaje = "18-23-20-19-20 8-15-21-4-3-6-19-5 22-12-19-23-21-16-19-20 22-25 5-3-10"
 print(combined(mensaje))
-
 
 
 mensaje = "4-16-19 4-23-12-19'5 4-9-12-20, 4-16-19 5-3-11-11-19-6'5 20-9-10-19"
